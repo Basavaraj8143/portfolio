69ad72545fc3be48b4c0c930e46f7146a8f088b1
Basavaraj <basavarajningasani123@gmail.com>
2026-02-27 17:06:58 +0530

    adding curser in process

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SplashCursor from "@/components/ui/SplashCursor";
 
 export const metadata: Metadata = {
   title: "Busss | Portfolio",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="light">
       <body className="antialiased">
+        <SplashCursor />
         {children}
       </body>
     </html>
